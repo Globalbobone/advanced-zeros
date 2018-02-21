ae53ad061f7c0689e813659e69d86bdbf0304091
Aliaksei Pliakhanau <3253255@rambler.ru>
2018-02-22 00:38:08 +0300

    part of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var count = 0; 
+  var expon = 1;
+  var n = base;
+
+  for (var i = 2; i < base; i++){
+    if (base % i == 0){
+      n = i;
+    }
+    
+  }
+  while (Math.pow(n, expon) < number){
+    count += parseInt(number / Math.pow(n, expon))
+    expon++;
+  }
+  return count;
 }
